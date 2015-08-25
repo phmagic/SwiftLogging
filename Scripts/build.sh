200ac7168d76116f8d9f175335e7a033d4bff45a
Jonathan Wight <jwight@mac.com>
2015-08-25 16:53:17 -0700

    Added support for buildkite

---
+++ Scripts/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PATH=$PATH:/usr/local/bin
+
+xcodebuild -version | grep "Xcode 7" > /dev/null || { echo 'Not running Xcode 7' ; exit 1; }
+
+cd `git rev-parse --show-toplevel`
+
+xctool -project SwiftLogging.xcodeproj -scheme All build test || exit $!
