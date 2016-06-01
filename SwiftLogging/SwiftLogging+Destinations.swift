7f5c21929805eeabd7ddb97c16c9ddd9fa7c6342
Jonathan Wight <jwight@mac.com>
2016-06-01 14:09:36 -0700

    Callback destination now passes entire event.

--- SwiftLogging/SwiftLogging+Destinations.swift
+++ SwiftLogging/SwiftLogging+Destinations.swift
@@ -13,16 +13,16 @@ import SwiftUtilities
 public class CallbackDestination: Destination {
     public let formatter: EventFormatter
 
-    public var callback: (String -> Void)?
+    public var callback: ((Event, String) -> Void)?
 
-    public init(formatter: EventFormatter = terseFormatter, callback: (String -> Void)? = nil) {
+    public init(formatter: EventFormatter = terseFormatter, callback: ((Event, String) -> Void)? = nil) {
         self.formatter = formatter
         self.callback = callback
     }
 
     public override func receiveEvent(event: Event) {
         let string = formatter(event) + "\n"
-        callback?(string)
+        callback?(event, string)
     }
 }
 
